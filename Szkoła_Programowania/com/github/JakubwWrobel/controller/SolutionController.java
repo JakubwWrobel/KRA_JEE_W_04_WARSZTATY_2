9402bb9ae733a30b7f1a7c68832a17a29f169fda
Jakub Wrobel <jakubwrobel22@gmail.com>
2020-01-29 15:47:11 +0100

    5

--- Szkoła_Programowania/com/github/JakubwWrobel/controller/SolutionController.java
+++ Szkoła_Programowania/com/github/JakubwWrobel/controller/SolutionController.java
@@ -1,7 +1,12 @@
 package com.github.JakubwWrobel.controller;
 
+import com.github.JakubwWrobel.addin.Checking;
+import com.github.JakubwWrobel.dao.ExerciseDAO;
 import com.github.JakubwWrobel.dao.SolutionDAO;
+import com.github.JakubwWrobel.dao.UserDAO;
+import com.github.JakubwWrobel.models.Exercise;
 import com.github.JakubwWrobel.models.Solution;
+import com.github.JakubwWrobel.models.User;
 
 
 import java.text.DateFormat;
@@ -13,9 +18,13 @@ import java.util.Scanner;
 
 public class SolutionController {
     private static SolutionDAO solutionDAO = new SolutionDAO();
+    private static ExerciseDAO exerciseDAO = new ExerciseDAO();
+    private static UserDAO userDAO = new UserDAO();
 
     public static void main(String[] args) {
         create();
+//        assignExerciseToSolution();
+//            assignUserToExercise();
     }
 
     private static void create() {
@@ -23,29 +32,117 @@ public class SolutionController {
         boolean running = true;
 
         while (running) {
-            System.out.println("Podaj datę utworzenia użytkownika: ");
-
-            DateFormat formatter = null;
-            Date convertedDate = null;
-
-            String yyyyMMdd = "20110914";
-            formatter =new SimpleDateFormat("yyyyMMdd");
-            try {
-                convertedDate =(Date) formatter.parse(yyyyMMdd);
-            } catch (ParseException e) {
-                e.printStackTrace();
+            System.out.println("Czy chcesz utworzyć nowe rozwiązanie?");
+            System.out.println("Yes/No");
+            String userInput = Checking.checkingString(scanner.nextLine()).toLowerCase();
+            if(userInput.equals("no")){
+                break;
+            }
+            else if(userInput.equals("yes")) {
+                Solution solution = new Solution();
+                solutionDAO.create(solution);
+                System.out.println(String.format("Zadanie zostało utworzne, jego ID to: %s\n", solution.getId()));
+                //ExerciseToSolution
+                System.out.println("Chcesz przypisać zadanie do tego rozwiązania?");
+                System.out.println("Yes/No");
+                String userInput2 = Checking.checkingString(scanner.nextLine()).toLowerCase();
+                if (userInput2.equals("yes")) {
+                    assignExerciseToSolutionAfterCreatingSolution(solution);
+                    running = false;
+                    scanner.close();
+                } else if(userInput2.equals("no")) {
+                    running = false;
+                    scanner.close();
+                }
             }
 
-            java.sql.Date sqlStartDate = new java.sql.Date(convertedDate.getTime());
-
-
-            Solution solution = new Solution(sqlStartDate, sqlStartDate, "new test");
-            solutionDAO.create(solution);
-
-
-            scanner.close();
-            running = false;
         }
 
     }
+
+
+    private static void assignExerciseToSolutionAfterCreatingSolution(Solution solution) {
+        boolean running = true;
+        while (running) {
+            System.out.println("Podaj ID zadania, które ma być przypisane do tego rozwiązania");
+            showAllExercises(exerciseDAO.findAll());
+            int userInput2 = Checking.checkingInt();
+            Exercise exercise = exerciseDAO.read(userInput2);
+            if (exercise == null) {
+            } else {
+                solutionDAO.insertExerciseIntoSolution(solution, userInput2);
+                System.out.println("Rozwiązanie zostało przypisane do zadania");
+                running = false;
+            }
+        }
+    }
+
+
+    //PRZEROBIĆ ABY POKAZYWAŁO ID
+    private static void assignExerciseToSolution() {
+        boolean running = true;
+        while (running) {
+            System.out.println("Podaj ID rozwiązania do którego chcesz przypisać zadanie: ");
+            solutionDAO.findAll();
+            int userInput = Checking.checkingInt();
+            Solution solution = solutionDAO.read(userInput);
+            if (solution == null) {
+                System.out.println("Rozwiązanie o takim ID nie istnieje");
+            } else {
+                while (running) {
+                    System.out.println("Podaj ID zadania, które ma być przypisane do tego rozwiązania");
+                    exerciseDAO.findAll();
+                    int userInput2 = Checking.checkingInt();
+                    Exercise exercise = exerciseDAO.read(userInput2);
+                    if (exercise == null) {
+                    } else {
+                        solutionDAO.insertExerciseIntoSolution(solution, userInput2);
+                        System.out.println("Rozwiązanie zostało przypisane do zadania");
+                        running = false;
+                    }
+                }
+            }
+        }
+    }
+
+    private static void assignUserToExercise() {
+        boolean running = true;
+        while (running) {
+            System.out.println("Podaj ID zadania, do którego chcesz przypisać do użytkownika");
+            solutionDAO.findAll();
+            Solution solution = solutionDAO.read(Checking.checkingInt());
+            if (solution == null) {
+
+            } else {
+                while (running) {
+                    System.out.println("Podaj ID użytkownika do którego chcesz przypisać zadanie: ");
+                    showAllUsers(userDAO.findAll());
+                    User user = userDAO.read(Checking.checkingInt());
+                    if (user == null) {
+
+                    } else {
+                        System.out.println("Zadanie zostało przypisane poniższemu użytkownikowi");
+                        solutionDAO.assignSolutionToTheUser(user, solution.getId());
+                        running = false;
+                    }
+                }
+            }
+
+        }
+    }
+
+    private static Exercise[] showAllExercises(Exercise[] exercises) {
+        for (int i = 0; i < exercises.length; i++) {
+            System.out.println(String.format("ID: %s\nTitle: %s\nDescription: %s\n", exercises[i].getId(), exercises[i].getTitle(), exercises[i].getDescription()));
+        }
+        return exercises;
+    }
+
+    private static User[] showAllUsers(User[] users) {
+        for (int i = 0; i < users.length; i++) {
+            System.out.println(String.format("ID: %s\nUsername: %s\nEmail: %s\n", users[i].getId(), users[i].getUsername(), users[i].getEmail()));
+        }
+        return users;
+    }
+
 }
--- Szkoła_Programowania/com/github/JakubwWrobel/dao/SolutionDAO.java
+++ Szkoła_Programowania/com/github/JakubwWrobel/dao/SolutionDAO.java
@@ -2,16 +2,18 @@ package com.github.JakubwWrobel.dao;
 
 import com.github.JakubwWrobel.addin.GetConnection;
 import com.github.JakubwWrobel.models.Solution;
+import com.github.JakubwWrobel.models.User;
 
 import java.sql.*;
+import java.util.Arrays;
 
 public class SolutionDAO {
     private static final String CREATE_SOLUTION_QUERY =
-            "INSERT INTO solution (created, updated, description) VALUES (?, ?, ?)";
+            "INSERT INTO solution () VALUES ()";
     private static final String READ_SOLUTION_QUERY =
             "SELECT * FROM solution where id = ?";
     private static final String UPDATE_SOLUTION_QUERY =
-            "UPDATE solution SET title = ?, description = ? where id = ?";
+            "UPDATE solution SET users_id = ?, created = NOW() where id = ?";
     private static final String DELETE_SOLUTION_QUERY =
             "DELETE FROM solution WHERE id = ?";
     private static final String FIND_ALL_SOLUTION_QUERY =
@@ -23,10 +25,7 @@ public class SolutionDAO {
     public Solution create(Solution solution) {
         try (Connection conn = GetConnection.getConnection()) {
             statement = conn.prepareStatement(CREATE_SOLUTION_QUERY, Statement.RETURN_GENERATED_KEYS);
-            statement.setDate(1, (Date) solution.getCreated());
-            statement.setDate(2, (Date) solution.getUpdated());
-            statement.setString(3, solution.getDescription());
-            statement.executeQuery();
+            statement.executeUpdate();
             ResultSet resultSet = statement.getGeneratedKeys();
             while (resultSet.next()) {
                 solution.setId(resultSet.getInt(1));
@@ -34,8 +33,8 @@ public class SolutionDAO {
             return solution;
         } catch (SQLException e) {
             System.out.println("Błąd połączenia z bazą");
+            return null;
         }
-        return null;
     }
 
     public void insertExerciseIntoSolution(Solution solution, int userInput) {
@@ -51,36 +50,71 @@ public class SolutionDAO {
     }
 
 
-    public void update() {
+    public void assignSolutionToTheUser(User user, int userInput) {
         try (Connection conn = GetConnection.getConnection()) {
-
+            statement = conn.prepareStatement(UPDATE_SOLUTION_QUERY);
+            statement.setInt(1, user.getId());
+            statement.setInt(2, userInput);
+            statement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Błąd połączenia z bazą");
         }
     }
-//    public Solution read(){
-//        try(Connection conn = GetConnection.getConnection()){
-//
-//        }catch (SQLException e){
-//            System.out.println("Błąd połączenia z bazą");
-//        }
-//    }
+    public Solution read(int userInput){
+        try(Connection conn = GetConnection.getConnection()){
+            statement = conn.prepareStatement(READ_SOLUTION_QUERY);
+            statement.setInt(1, userInput);
+            ResultSet resultSet = statement.executeQuery();
+            if(resultSet.next()){
+                Solution solution = new Solution();
+                solution.setId(resultSet.getInt("id"));
+                return solution;
+            }else {
+                return null;
+            }
+        }catch (SQLException e){
+            System.out.println("Błąd połączenia z bazą");
+        }
+        return null;
+    }
 
-    public void delete() {
+    public int delete() {
         try (Connection conn = GetConnection.getConnection()) {
-
+            statement = conn.prepareStatement(DELETE_SOLUTION_QUERY);
+            statement.executeUpdate();
+            return 1;
         } catch (SQLException e) {
             System.out.println("Błąd połączenia z bazą");
         }
+        return -1;
     }
 
-//    public Solution[] findAll(){
-//        try(Connection conn = GetConnection.getConnection()){
-//
-//        }catch (SQLException e){
-//            System.out.println("Błąd połączenia z bazą");
-//        }
-//    }
+    public Solution[] findAll(){
+        try(Connection conn = GetConnection.getConnection()){
+            statement = conn.prepareStatement(FIND_ALL_SOLUTION_QUERY);
+            ResultSet resultSet = statement.executeQuery();
+            Solution[] solutions = new Solution[0];
+            while(resultSet.next()){
+                Solution solution = new Solution();
+                solution.setId(resultSet.getInt("id"));
+                solutions = addToArray(solutions,solution);
+            }
+
+            for(int i = 0; i < solutions.length; i++){
+                System.out.println(String.format("ID: %s\n", solutions[i].getId()));
+            }
+            return solutions;
+        }catch (SQLException e){
+            System.out.println("Błąd połączenia z bazą");
+        }
+        return null;
+    }
+
+    private Solution[]addToArray(Solution[] solutions, Solution solution){
+        Solution[] tempSolutions = Arrays.copyOf(solutions, solutions.length + 1);
+        tempSolutions[tempSolutions.length - 1] = solution;
+        return tempSolutions;
+    }
 
 
 }
